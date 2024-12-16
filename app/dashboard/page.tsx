be43176211333534d2d6b1a93f7af1aa920a4519
notbnull <brandonreesenull@gmail.com>
2024-12-16 15:13:22 -0600

    timeout on mongo

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,9 @@
 import { Suspense } from "react";
-import SearchBar from "@/app/ui/dashboard/search-bar";
 import { getMarketData } from "@/app/lib/getMarketData";
-import { getTrendingCards } from "@/app/lib/getTrendingCards";
 import MarketOverview from "@/app/ui/dashboard/market-overview";
 import TrendingCards from "@/app/ui/dashboard/trending-cards";
 import MarketTrends from "@/app/ui/dashboard/market-trends";
+import { getTrendingCards } from "../lib/getTrendingCards";
 
 export default async function Dashboard() {
   const marketDataPromise = getMarketData();
